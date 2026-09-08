7612d6b48040869d12d6d3e26f41b2c04bd946ce
jagnyadatta <jagnyadattadalai99215@gmail.com>
2026-09-08 17:30:09 +0530

    Added User Entity

---
+++ backend/src/main/java/com/jagnyadatta/NextMovie/entity/User.java
@@ -0,0 +1,60 @@
+package com.jagnyadatta.NextMovie.entity;
+
+import com.jagnyadatta.NextMovie.enums.Role;
+import com.jagnyadatta.NextMovie.enums.UserStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role = Role.USER;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status = UserStatus.ACTIVE;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
---
+++ backend/src/main/java/com/jagnyadatta/NextMovie/enums/Role.java
@@ -0,0 +1,7 @@
+package com.jagnyadatta.NextMovie.enums;
+
+public enum Role {
+    USER,
+    ADMIN,
+    THEATER_ADMIN
+}
