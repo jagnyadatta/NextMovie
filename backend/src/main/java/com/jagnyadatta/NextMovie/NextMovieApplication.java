dfb5708ae46fa8a6efd23cfb2c196509a9db8f1d
jagnyadatta <jagnyadattadalai99215@gmail.com>
2026-09-08 16:45:45 +0530

    Initialized full project

---
+++ backend/src/main/java/com/jagnyadatta/NextMovie/NextMovieApplication.java
@@ -0,0 +1,13 @@
+package com.jagnyadatta.NextMovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextMovieApplication.class, args);
+	}
+
+}
